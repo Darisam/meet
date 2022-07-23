77ad35abd5b9051a5706e34b80a5a2b2f2937b56
Darisam <98291008+Darisam@users.noreply.github.com>
2022-07-23 22:35:47 +0200

    Load the calendar events  from google
    And the comment it out because it doesn't work just yet

--- src/api.js
+++ src/api.js
@@ -1,18 +1,101 @@
+import { calendarEventList } from './mock-data';
+/*
+const checkToken = async (accessToken) => {
+  const result = await fetch(
+    `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
+  )
+    .then((response) => response.json())
+    .catch((error) => error.json());
+  return result;
+};
+
+const getToken = async (code) => {
+  const encodeCode = encodeURIComponent(code);
+  const { access_token } = await fetch(
+    'https://mpsz2rdhq1.execute-api.eu-central-1.amazonaws.com/dev/api/token/' +
+      encodeCode
+  )
+    .then((response) => response.json())
+    .catch((error) => error);
+
+  if (access_token) {
+    localStorage.setItem('access_token', access_token);
+  }
+
+  return access_token;
+};
+
+const getAccessToken = async () => {
+  const accessToken = localStorage.getItem('access_token');
+  const tokenCheck = accessToken && (await checkToken(accessToken));
+
+  if (!accessToken || tokenCheck.error) {
+    await localStorage.removeItem('access_token');
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = await searchParams.get('code');
+    if (!code) {
+      const results = await axios.get(
+        'https://mpsz2rdhq1.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url'
+      );
+      const { authUrl } = results.data;
+      return (window.location.href = authUrl);
+    }
+    return code && getToken(code);
+  }
+  return accessToken;
+};
+
+const removeQuery = () => {
+  if (window.history.pushState && window.location.pathname) {
+    let newUrl =
+      window.location.protocol +
+      '//' +
+      window.location.host +
+      window.location.pathname;
+    window.history.pushState('', '', newUrl);
+  } else {
+    let newUrl = window.location.protocol + '//' + window.location.host;
+    window.history.pushState('', '', newUrl);
+  }
+};
+
+const getEvents = async () => {
+  NProgress.start();
+
+  if (window.location.href.startsWith('http://localhost')) {
+    NProgress.done();
+    return calendarEventList;
+  }
+
+  const token = await getAccessToken();
+
+  if (token) {
+    removeQuery();
+    const url =
+      'https://mpsz2rdhq1.execute-api.eu-central-1.amazonaws.com/dev/api/get-events/' +
+      token;
+    const result = await axios.get(url);
+    if (result.data) {
+      let locations = extractLocations(result.data.events);
+      localStorage.setItem('lastEvents', JSON.stringify(result.data));
+      localStorage.setItem('locations', JSON.stringify(locations));
+    }
+    NProgress.done();
+    return result.data.events;
+  }
+};
+
+
+export { extractLocations, getEvents, getAccessToken };
+*/
+const getEvents = () => {
+  return calendarEventList;
+};
+
 const extractLocations = (events) => {
   const locationsList = events.map((event) => event.location);
   const locations = [...new Set(locationsList)];
   return locations;
 };
 
-const extractDetails = (event) => {
-  const eventDetails = {
-    summary: event.summary,
-    description: event.description,
-    location: event.location,
-    htmlLink: event.htmlLink,
-    startTime: event.start.dateTime,
-  };
-  return eventDetails;
-};
-
-export { extractLocations, extractDetails };
+export { extractLocations, getEvents };
