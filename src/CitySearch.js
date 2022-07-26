29b3a34ef15a9ce74ecc7827e59b74fb56cf4302
Darisam <98291008+Darisam@users.noreply.github.com>
2022-07-27 01:32:36 +0200

    Integrate Alert Components

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
+import { InfoAlert } from './Alert';
 
 class CitySearch extends Component {
-  state = { query: '', suggestions: [], showSuggestions: undefined };
+  state = {
+    query: '',
+    suggestions: [],
+    showSuggestions: undefined,
+    infoText: '',
+  };
 
   handleInputChange = (event) => {
     const value = event.target.value;
     const suggestions = this.props.locations.filter((location) => {
       return location.toUpperCase().startsWith(value.toUpperCase());
     });
-    this.setState({ query: value, suggestions: suggestions });
+    if (suggestions.length === 0) {
+      this.setState({
+        query: value,
+        infoText:
+          'We can not find the city you are looking for. Please try another city.',
+      });
+    } else {
+      this.setState({ query: value, suggestions: suggestions, infoText: '' });
+    }
   };
 
   handleItemClick = (suggestion) => {
@@ -16,6 +30,7 @@ class CitySearch extends Component {
       query: suggestion,
       suggestions: [],
       showSuggestions: false,
+      infoText: '',
     });
     this.props.updateLocation(suggestion);
   };
@@ -23,8 +38,11 @@ class CitySearch extends Component {
   render() {
     return (
       <div className="CitySearch">
-        <div>
-          <label for="city"> Search for a city </label>
+        <div className="InfoAlert">
+          <InfoAlert text={this.state.infoText} />
+        </div>
+        <div className="searchBox">
+          <label htmlFor="city"> Search for a city </label>
           <input
             type="text"
             id="city"
--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,20 +1,31 @@
 import React, { Component } from 'react';
+import { ErrorAlert } from './Alert';
 
 class NumberOfEvents extends Component {
-  state = { query: 32 };
+  state = { query: 32, errorText: '' };
 
   handleInputChange = (event) => {
     let input = event.target.value;
     this.setState({ query: input });
-    this.props.updateNumber(input);
+    let numberInput = Number(input);
+
+    if (input === '') {
+      this.setState({ errorText: '' });
+    } else if (Number.isInteger(numberInput) && numberInput >= 0) {
+      this.setState({ errorText: '' });
+      this.props.updateNumber(numberInput);
+    } else {
+      this.setState({ errorText: 'Please enter a positive integer.' });
+    }
   };
 
   render() {
     return (
       <div className="number-of-events">
-        <label for="number-input">Number of Events</label>
+        <ErrorAlert text={this.state.errorText} />
+        <label htmlFor="number-input">Number of Events</label>
         <input
-          type="number"
+          type="text"
           id="number-input"
           className="number-input"
           value={this.state.query}
